1d4f1ad3cbcc0d036fa4c99822927375a341f9ce
DaniDMoura <danilocraft933@gmail.com>
2026-05-08 18:32:45 -0300

    refactor: update header design

--- src/app/layout/header/nav-data.ts
+++ src/app/layout/header/nav-data.ts
@@ -9,8 +9,13 @@ export const NAVIGATION_DATA: NavItem[] = [
   {
     label: 'O Escotismo',
     children: [
-      { label: 'O que é o Escotismo', route: '/escotismo' },
-      { label: 'Lei e Promessa', route: '/escotismo/lei-promessa' },
+      {
+        label: 'O Movimento',
+        children: [
+          { label: 'O que é o Escotismo', route: '/escotismo' },
+          { label: 'Lei e Promessa', route: '/escotismo/lei-promessa' },
+        ],
+      },
       {
         label: 'Ramos Escoteiros',
         children: [
@@ -18,35 +23,54 @@ export const NAVIGATION_DATA: NavItem[] = [
           { label: 'Escoteiro', route: '/escotismo/ramos/escoteiro' },
           { label: 'Sênior', route: '/escotismo/ramos/senior' },
           { label: 'Pioneiro', route: '/escotismo/ramos/pioneiro' },
-        ]
+        ],
       },
-      { label: 'História do Movimento', route: '/escotismo/historia-do-movimento' },
-    ]
+      {
+        label: 'História',
+        children: [{ label: 'História do Movimento', route: '/escotismo/historia-do-movimento' }],
+      },
+    ],
   },
   {
     label: 'Quem Somos',
     children: [
-      { label: 'Nossa História', route: '/grupo/nossa-historia' },
-      { label: 'Chefia', route: '/grupo/chefia' },
-      { label: 'Diretoria', route: '/grupo/diretoria' },
-      { label: 'Nossa Sede', route: '/grupo/sede' },
-    ]
+      {
+        label: 'História',
+        children: [{ label: 'Nossa História', route: '/grupo/nossa-historia' }],
+      },
+      {
+        label: 'Sobre Nós',
+        children: [
+          { label: 'Chefia', route: '/grupo/chefia' },
+          { label: 'Diretoria', route: '/grupo/diretoria' },
+        ],
+      },
+      {
+        label: 'Sede',
+        children: [{ label: 'Nossa Sede', route: '/grupo/sede/' }],
+      },
+    ],
   },
   {
     label: 'Galeria',
     children: [
-      { label: 'Eventos', route: '/galeria/eventos' },
-      { label: 'Acampamentos', route: '/galeria/acampamentos' },
-      { label: 'Atividades na Sede', route: '/galeria/atividades-sede' },
-    ]
+      {
+        label: 'Fotos',
+        children: [
+          { label: 'Eventos', route: '/galeria/eventos' },
+          { label: 'Acampamentos', route: '/galeria/acampamentos' },
+          { label: 'Atividades na Sede', route: '/galeria/atividades-sede' },
+        ],
+      },
+    ],
   },
   {
     label: 'Fale Conosco',
-    route: '/fale-conosco'
+    route: '/fale-conosco',
   },
   {
     label: 'Seja Escoteiro',
     route: '/seja-escoteiro',
-    isCTA: true
-  }
+    isCTA: true,
+  },
 ];
