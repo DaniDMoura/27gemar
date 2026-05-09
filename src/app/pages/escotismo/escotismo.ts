f3b5cc057b264713d46e514d2f3360db22c354af
DaniDMoura <danilocraft933@gmail.com>
2026-05-08 21:57:07 -0300

    style: refactor ui at /escotismo

--- src/app/pages/escotismo/escotismo.ts
+++ src/app/pages/escotismo/escotismo.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Breadcrumb } from '../../shared/components/breadcrumb/breadcrumb';
 import { YoutubePlayer } from "../../shared/components/youtube-player/youtube-player";
 
 @Component({
   selector: 'app-escotismo',
   standalone: true,
-  imports: [Breadcrumb, YoutubePlayer],
+  imports: [Breadcrumb, YoutubePlayer, RouterLink],
   templateUrl: './escotismo.html',
   styleUrl: './escotismo.scss'
 })
